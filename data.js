16ebc74bf872992eff43c4ff9efaefe671014f5f
gabrielsivally <gabriellsg17@gmail.com>
2026-07-13 20:23:55 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,225 +1,807 @@
+// ==============================
+// GABIPEDIA DATA
+// ==============================
+
+
 const categorias = [
 
     {
-        id: "pokemon",
-        nome: "Pokémon",
-        imagem: "assets/pokemon.jpg"
+        id:"pokemon",
+        nome:"Pokémon",
+        imagem:"assets/pokemon.jpg"
     },
 
     {
-        id: "anime",
-        nome: "Animes",
-        imagem: "assets/anime.jpg"
+        id:"anime",
+        nome:"Animes",
+        imagem:"assets/anime.jpg"
     },
 
     {
-        id: "manga",
-        nome: "Mangás",
-        imagem: "assets/manga.jpg"
+        id:"manga",
+        nome:"Mangás",
+        imagem:"assets/manga.jpg"
     },
 
     {
-        id: "personagens",
-        nome: "Personagens",
-        imagem: "assets/personagens.jpg"
+        id:"personagens",
+        nome:"Personagens",
+        imagem:"assets/personagens.jpg"
     },
 
     {
-        id: "musica",
-        nome: "Música",
-        imagem: "assets/music.jpg"
+        id:"musica",
+        nome:"Música",
+        imagem:"assets/music.jpg"
     },
 
     {
-        id: "jogos",
-        nome: "Jogos",
-        imagem: "assets/games.jpg"
+        id:"jogos",
+        nome:"Jogos",
+        imagem:"assets/games.jpg"
     },
 
     {
-        id: "series",
-        nome: "Séries",
-        imagem: "assets/series.jpg"
+        id:"series",
+        nome:"Séries",
+        imagem:"assets/series.jpg"
     },
 
     {
-        id: "filmes",
-        nome: "Filmes",
-        imagem: "assets/movie.jpg"
+        id:"filmes",
+        nome:"Filmes",
+        imagem:"assets/movie.jpg"
     },
 
     {
-        id: "comidas",
-        nome: "Comidas",
-        imagem: "assets/food.jpg"
+        id:"comidas",
+        nome:"Comidas",
+        imagem:"assets/food.jpg"
     },
 
     {
-        id: "esportes",
-        nome: "Esportes",
-        imagem: "assets/sports.jpg"
+        id:"esportes",
+        nome:"Esportes",
+        imagem:"assets/sports.jpg"
     },
 
     {
-        id: "interesses",
-        nome: "Interesses",
-        imagem: "assets/interests.jpg"
+        id:"interesses",
+        nome:"Interesses",
+        imagem:"assets/interests.jpg"
     }
 
 ];
 
+
+
+
+
 const dados = {
 
-    pokemon:{
 
-        nome:"🐉 Pokémon",
 
-        itens:[
+pokemon:{
 
-            {
-                nome:"Favoritos",
-                imagem:"assets/folders/favoritos.jpg"
-            }
+    nome:"🐉 Pokémon",
 
-        ]
+    itens:[
 
-    },
+        {
+            nome:"Oshawott",
+            imagem:"assets/pokemon/oshawott.jpg"
+        },
 
-    anime:{
+        {
+            nome:"Garchomp",
+            imagem:"assets/pokemon/garchomp.jpg"
+        },
 
-        nome:"📺 Animes",
+        {
+            nome:"Kyogre",
+            imagem:"assets/pokemon/kyogre.jpg"
+        },
 
-        itens:[
+        {
+            nome:"Silvally",
+            imagem:"assets/pokemon/silvally.jpg"
+        },
 
-            {
-                nome:"Favoritos",
-                imagem:"assets/folders/favoritos.jpg"
-            }
+        {
+            nome:"Gengar",
+            imagem:"assets/pokemon/gengar.jpg"
+        },
 
-        ]
+        {
+            nome:"Zapdos",
+            imagem:"assets/pokemon/zapdos.jpg"
+        }
 
-    },
+    ]
 
-    manga:{
+},
 
-        nome:"📖 Mangás",
 
-        itens:[
 
-            {
-                nome:"Favoritos",
-                imagem:"assets/folders/favoritos.jpg"
-            }
 
-        ]
 
-    },
+anime:{
 
-    personagens:{
+    nome:"📺 Animes",
 
-        nome:"👤 Personagens",
+    itens:[
 
-        itens:[
+        {
+            nome:"One Piece",
+            imagem:"assets/animes/onepiece.jpg"
+        },
 
-            {
-                nome:"Todos",
-                imagem:"assets/folders/personagens.jpg"
-            }
+        {
+            nome:"Fullmetal Alchemist",
+            imagem:"assets/animes/fullmetal.jpg"
+        },
 
-        ]
+        {
+            nome:"Mob Psycho 100",
+            imagem:"assets/animes/mob.jpg"
+        },
 
-    },
+        {
+            nome:"Parasyte",
+            imagem:"assets/animes/parasyte.jpg"
+        },
 
-    musica:{
+        {
+            nome:"Bungo Stray Dogs",
+            imagem:"assets/animes/bungo.jpg"
+        },
 
-        nome:"🎵 Música",
+        {
+            nome:"Death Note",
+            imagem:"assets/animes/deathnote.jpg"
+        },
 
-        itens:[
+        {
+            nome:"Cyberpunk Edgerunners",
+            imagem:"assets/animes/cyberpunk.jpg"
+        }
 
-            {
-                nome:"Músicas",
-                imagem:"assets/folders/musicas.jpg"
-            },
+    ]
 
-            {
-                nome:"Artistas",
-                imagem:"assets/folders/artistas.jpg"
-            },
+},
 
-            {
-                nome:"Álbuns",
-                imagem:"assets/folders/albuns.jpg"
-            }
+    // ==============================
+// MANGÁS
+// ==============================
 
-        ]
 
-    },
+manga:{
 
-    jogos:{
+    nome:"📖 Mangás",
 
-        nome:"🎮 Jogos",
+    itens:[
 
-        itens:[
+        {
+            nome:"One Piece",
+            imagem:"assets/mangas/onepiece.jpg"
+        },
 
-            {
-                nome:"Favoritos",
-                imagem:"assets/folders/jogos.jpg"
-            }
+        {
+            nome:"The Climber",
+            imagem:"assets/mangas/theclimber.jpg"
+        },
 
-        ]
+        {
+            nome:"Slam Dunk",
+            imagem:"assets/mangas/slamdunk.jpg"
+        },
 
-    },
+        {
+            nome:"Vagabond",
+            imagem:"assets/mangas/vagabond.jpg"
+        },
 
-    series:{
+        {
+            nome:"Solo Leveling",
+            imagem:"assets/mangas/sololeveling.jpg"
+        },
 
-        nome:"📺 Séries",
+        {
+            nome:"Chainsaw Man",
+            imagem:"assets/mangas/chainsawman.jpg"
+        }
 
-        itens:[
+    ]
 
-            {
-                nome:"Favoritas",
-                imagem:"assets/folders/series.jpg"
-            }
+},
 
-        ]
 
-    },
 
-    filmes:{
 
-        nome:"🎬 Filmes",
+// ==============================
+// PERSONAGENS
+// ==============================
 
-        itens:[
 
-            {
-                nome:"Favoritos",
-                imagem:"assets/folders/filmes.jpg"
-            }
+personagens:{
 
-        ]
+    nome:"👤 Personagens",
 
-    },
+    itens:[
 
-    comidas:{
+        {
+            nome:"Sanji",
+            imagem:"assets/personagens/sanji.jpg"
+        },
 
-        nome:"🍜 Comidas",
+        {
+            nome:"Aokiji",
+            imagem:"assets/personagens/aokiji.jpg"
+        },
 
-        itens:[
+        {
+            nome:"Ranpo",
+            imagem:"assets/personagens/ranpo.jpg"
+        },
 
-            {
-                nome:"Favoritas",
-                imagem:"assets/folders/comidas.jpg"
-            }
+        {
+            nome:"Kana Kimishima",
+            imagem:"assets/personagens/kana.jpg"
+        },
 
-        ]
+        {
+            nome:"L",
+            imagem:"assets/personagens/l.jpg"
+        },
 
-    },
+        {
+            nome:"Luffy",
+            imagem:"assets/personagens/luffy.jpg"
+        },
 
-    esportes:{
+        {
+            nome:"Robin",
+            imagem:"assets/personagens/robin.jpg"
+        },
 
-        nome:"⚽ Esportes",
+        {
+            nome:"Kaiser",
+            imagem:"assets/personagens/kaiser.jpg"
+        },
 
-        itens:[
+        {
+            nome:"Dazai",
+            imagem:"assets/personagens/dazai.jpg"
+        },
 
-            {
-               
+        {
+            nome:"Shigeo Kageyama",
+            imagem:"assets/personagens/shigeo.jpg"
+        }
+
+    ]
+
+},
+
+
+
+
+// ==============================
+// MÚSICA
+// ==============================
+
+
+musica:{
+
+    nome:"🎵 Música",
+
+    itens:[
+
+        {
+            id:"musicas",
+            nome:"Músicas favoritas",
+            imagem:"assets/folders/musicas.jpg"
+        },
+
+        {
+            id:"artistas",
+            nome:"Artistas favoritos",
+            imagem:"assets/folders/artistas.jpg"
+        },
+
+        {
+            id:"albuns",
+            nome:"Álbuns favoritos",
+            imagem:"assets/folders/albuns.jpg"
+        }
+
+    ]
+
+},
+
+
+
+musicas:{
+
+    nome:"🎧 Músicas favoritas",
+
+    itens:[
+
+        {
+            nome:"Laços de Redenção",
+            imagem:"assets/musica/lacos.jpg"
+        },
+
+        {
+            nome:"Good Things Go",
+            imagem:"assets/musica/goodthingsgo.jpg"
+        },
+
+        {
+            nome:"Roda Gigante",
+            imagem:"assets/musica/rodagigante.jpg"
+        },
+
+        {
+            nome:"Marineford",
+            imagem:"assets/musica/marineford.jpg"
+        },
+
+        {
+            nome:"Inevitável Destino",
+            imagem:"assets/musica/inevitavel.jpg"
+        }
+
+    ]
+
+},
+
+
+
+artistas:{
+
+    nome:"🎤 Artistas favoritos",
+
+    itens:[
+
+        {
+            nome:"Henrique Mendonça",
+            imagem:"assets/artistas/henrique.jpg"
+        },
+
+        {
+            nome:"M4rkim",
+            imagem:"assets/artistas/m4rkim.jpg"
+        },
+
+        {
+            nome:"Rodrigo Zin",
+            imagem:"assets/artistas/rodrigo.jpg"
+        },
+
+        {
+            nome:"Linkin Park",
+            imagem:"assets/artistas/linkinpark.jpg"
+        },
+
+        {
+            nome:"Akashi Cruz",
+            imagem:"assets/artistas/akashi.jpg"
+        }
+
+    ]
+
+},
+
+
+
+albuns:{
+
+    nome:"💿 Álbuns favoritos",
+
+    itens:[
+
+        {
+            nome:"From Zero",
+            imagem:"assets/albuns/fromzero.jpg"
+        },
+
+        {
+            nome:"Laços",
+            imagem:"assets/albuns/lacos.jpg"
+        },
+
+        {
+            nome:"Invasor",
+            imagem:"assets/albuns/invasor.jpg"
+        }
+
+    ]
+
+},
+
+    // ==============================
+// JOGOS
+// ==============================
+
+
+jogos:{
+
+    nome:"🎮 Jogos",
+
+    itens:[
+
+        {
+            nome:"Transformice",
+            imagem:"assets/jogos/transformice.jpg"
+        },
+
+        {
+            id:"cs2",
+            nome:"Counter Strike 2",
+            imagem:"assets/jogos/cs2.jpg"
+        },
+
+        {
+            id:"pokemonjogos",
+            nome:"Pokémon",
+            imagem:"assets/jogos/pokemon.jpg"
+        },
+
+        {
+            nome:"Paciência",
+            imagem:"assets/jogos/paciencia.jpg"
+        },
+
+        {
+            nome:"Cuphead",
+            imagem:"assets/jogos/cuphead.jpg"
+        }
+
+    ]
+
+},
+
+
+
+cs2:{
+
+    nome:"🔫 Counter Strike 2",
+
+    itens:[
+
+        {
+            nome:"FURIA",
+            imagem:"assets/jogos/furia.jpg"
+        },
+
+        {
+            nome:"FaZe",
+            imagem:"assets/jogos/faze.jpg"
+        }
+
+    ]
+
+},
+
+
+
+pokemonjogos:{
+
+    nome:"🎮 Jogos Pokémon",
+
+    itens:[
+
+        {
+            nome:"Fire Red",
+            imagem:"assets/jogos/fire-red.jpg"
+        },
+
+        {
+            nome:"Emerald",
+            imagem:"assets/jogos/emerald.jpg"
+        },
+
+        {
+            nome:"Legends Arceus",
+            imagem:"assets/jogos/arceus.jpg"
+        },
+
+        {
+            nome:"Radical Red",
+            imagem:"assets/jogos/radical-red.jpg"
+        },
+
+        {
+            nome:"Pokémon GO",
+            imagem:"assets/jogos/pokemon-go.jpg"
+        }
+
+    ]
+
+},
+
+
+
+
+
+// ==============================
+// SÉRIES
+// ==============================
+
+
+series:{
+
+    nome:"📺 Séries favoritas",
+
+    itens:[
+
+        {
+            nome:"Sherlock",
+            imagem:"assets/series/sherlock.jpg"
+        },
+
+        {
+            nome:"The Mentalist",
+            imagem:"assets/series/mentalist.jpg"
+        },
+
+        {
+            nome:"Lupin",
+            imagem:"assets/series/lupin.jpg"
+        },
+
+        {
+            nome:"Gambito da Rainha",
+            imagem:"assets/series/gambito.jpg"
+        },
+
+        {
+            nome:"Teen Wolf",
+            imagem:"assets/series/teenwolf.jpg"
+        },
+
+        {
+            nome:"Flash",
+            imagem:"assets/series/flash.jpg"
+        }
+
+    ]
+
+},
+
+
+
+
+
+// ==============================
+// FILMES
+// ==============================
+
+
+filmes:{
+
+    nome:"🎬 Filmes favoritos",
+
+    itens:[
+
+        {
+            nome:"Eu Quero Comer Seu Pâncreas",
+            imagem:"assets/filmes/pancreas.jpg"
+        },
+
+        {
+            nome:"One Piece Stampede",
+            imagem:"assets/filmes/stampede.jpg"
+        },
+
+        {
+            nome:"Truque de Mestre",
+            imagem:"assets/filmes/truquedemestre.jpg"
+        },
+
+        {
+            nome:"The First Slam Dunk",
+            imagem:"assets/filmes/slamdunk.jpg"
+        }
+
+    ]
+
+},
+
+    // ==============================
+// COMIDAS
+// ==============================
+
+
+comidas:{
+
+    nome:"🍜 Comidas",
+
+    itens:[
+
+        {
+            nome:"Macarrão com carne moída",
+            imagem:"assets/comidas/macarrao.jpg"
+        },
+
+        {
+            nome:"Guaravita",
+            imagem:"assets/comidas/guaravita.jpg"
+        },
+
+        {
+            nome:"Nesquik",
+            imagem:"assets/comidas/nesquik.jpg"
+        },
+
+        {
+            nome:"Pão de alho",
+            imagem:"assets/comidas/paoalho.jpg"
+        }
+
+    ]
+
+},
+
+
+
+
+
+// ==============================
+// ESPORTES
+// ==============================
+
+
+esportes:{
+
+    nome:"⚽ Esportes",
+
+    itens:[
+
+        {
+            nome:"Xadrez",
+            imagem:"assets/esportes/xadrez.jpg"
+        },
+
+        {
+            id:"basquete",
+            nome:"Basquete",
+            imagem:"assets/esportes/basquete.jpg"
+        },
+
+        {
+            id:"futebol",
+            nome:"Futebol",
+            imagem:"assets/esportes/futebol.jpg"
+        },
+
+        {
+            nome:"Alpinismo",
+            imagem:"assets/esportes/alpinismo.jpg"
+        },
+
+        {
+            nome:"Skate",
+            imagem:"assets/esportes/skate.jpg"
+        }
+
+    ]
+
+},
+
+
+
+basquete:{
+
+    nome:"🏀 Basquete",
+
+    itens:[
+
+        {
+            nome:"Chicago Bulls",
+            imagem:"assets/esportes/bulls.jpg"
+        },
+
+        {
+            nome:"Steve Kerr",
+            imagem:"assets/esportes/stevekerr.jpg"
+        }
+
+    ]
+
+},
+
+
+
+futebol:{
+
+    nome:"⚽ Futebol",
+
+    itens:[
+
+        {
+            nome:"Flamengo",
+            imagem:"assets/esportes/flamengo.jpg"
+        },
+
+        {
+            nome:"Neymar",
+            imagem:"assets/esportes/neymar.jpg"
+        },
+
+        {
+            nome:"Zico",
+            imagem:"assets/esportes/zico.jpg"
+        },
+
+        {
+            nome:"Arrascaeta",
+            imagem:"assets/esportes/arrascaeta.jpg"
+        }
+
+    ]
+
+},
+
+
+
+
+
+// ==============================
+// INTERESSES
+// ==============================
+
+
+interesses:{
+
+    nome:"✨ Outras coisas",
+
+    itens:[
+
+        {
+            nome:"Lontra",
+            imagem:"assets/interesses/lontra.jpg"
+        },
+
+        {
+            nome:"Frio",
+            imagem:"assets/interesses/frio.jpg"
+        },
+
+        {
+            nome:"Desenhar",
+            imagem:"assets/interesses/desenhar.jpg"
+        },
+
+        {
+            nome:"Margarida",
+            imagem:"assets/interesses/margarida.jpg"
+        },
+
+        {
+            nome:"Fones",
+            imagem:"assets/interesses/fones.jpg"
+        },
+
+        {
+            nome:"Islândia",
+            imagem:"assets/interesses/islandia.jpg"
+        },
+
+        {
+            nome:"China",
+            imagem:"assets/interesses/china.jpg"
+        },
+
+        {
+            nome:"Cardistry",
+            imagem:"assets/interesses/cardistry.jpg"
+        }
+
+    ]
+
+}
+
+
+};
