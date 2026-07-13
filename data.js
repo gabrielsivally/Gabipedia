8baf53e5ca0692e412dbc6fdd2c349ce654fe0b9
gabrielsivally <gabriellsg17@gmail.com>
2026-07-13 18:59:34 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,225 @@
+const categorias = [
+
+    {
+        id: "pokemon",
+        nome: "Pokémon",
+        imagem: "assets/pokemon.jpg"
+    },
+
+    {
+        id: "anime",
+        nome: "Animes",
+        imagem: "assets/anime.jpg"
+    },
+
+    {
+        id: "manga",
+        nome: "Mangás",
+        imagem: "assets/manga.jpg"
+    },
+
+    {
+        id: "personagens",
+        nome: "Personagens",
+        imagem: "assets/personagens.jpg"
+    },
+
+    {
+        id: "musica",
+        nome: "Música",
+        imagem: "assets/music.jpg"
+    },
+
+    {
+        id: "jogos",
+        nome: "Jogos",
+        imagem: "assets/games.jpg"
+    },
+
+    {
+        id: "series",
+        nome: "Séries",
+        imagem: "assets/series.jpg"
+    },
+
+    {
+        id: "filmes",
+        nome: "Filmes",
+        imagem: "assets/movie.jpg"
+    },
+
+    {
+        id: "comidas",
+        nome: "Comidas",
+        imagem: "assets/food.jpg"
+    },
+
+    {
+        id: "esportes",
+        nome: "Esportes",
+        imagem: "assets/sports.jpg"
+    },
+
+    {
+        id: "interesses",
+        nome: "Interesses",
+        imagem: "assets/interests.jpg"
+    }
+
+];
+
+const dados = {
+
+    pokemon:{
+
+        nome:"🐉 Pokémon",
+
+        itens:[
+
+            {
+                nome:"Favoritos",
+                imagem:"assets/folders/favoritos.jpg"
+            }
+
+        ]
+
+    },
+
+    anime:{
+
+        nome:"📺 Animes",
+
+        itens:[
+
+            {
+                nome:"Favoritos",
+                imagem:"assets/folders/favoritos.jpg"
+            }
+
+        ]
+
+    },
+
+    manga:{
+
+        nome:"📖 Mangás",
+
+        itens:[
+
+            {
+                nome:"Favoritos",
+                imagem:"assets/folders/favoritos.jpg"
+            }
+
+        ]
+
+    },
+
+    personagens:{
+
+        nome:"👤 Personagens",
+
+        itens:[
+
+            {
+                nome:"Todos",
+                imagem:"assets/folders/personagens.jpg"
+            }
+
+        ]
+
+    },
+
+    musica:{
+
+        nome:"🎵 Música",
+
+        itens:[
+
+            {
+                nome:"Músicas",
+                imagem:"assets/folders/musicas.jpg"
+            },
+
+            {
+                nome:"Artistas",
+                imagem:"assets/folders/artistas.jpg"
+            },
+
+            {
+                nome:"Álbuns",
+                imagem:"assets/folders/albuns.jpg"
+            }
+
+        ]
+
+    },
+
+    jogos:{
+
+        nome:"🎮 Jogos",
+
+        itens:[
+
+            {
+                nome:"Favoritos",
+                imagem:"assets/folders/jogos.jpg"
+            }
+
+        ]
+
+    },
+
+    series:{
+
+        nome:"📺 Séries",
+
+        itens:[
+
+            {
+                nome:"Favoritas",
+                imagem:"assets/folders/series.jpg"
+            }
+
+        ]
+
+    },
+
+    filmes:{
+
+        nome:"🎬 Filmes",
+
+        itens:[
+
+            {
+                nome:"Favoritos",
+                imagem:"assets/folders/filmes.jpg"
+            }
+
+        ]
+
+    },
+
+    comidas:{
+
+        nome:"🍜 Comidas",
+
+        itens:[
+
+            {
+                nome:"Favoritas",
+                imagem:"assets/folders/comidas.jpg"
+            }
+
+        ]
+
+    },
+
+    esportes:{
+
+        nome:"⚽ Esportes",
+
+        itens:[
+
+            {
+               
